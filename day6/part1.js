555041971842294520d206f34bb598fabb18daa6
tjankowski <tomasz.jankowski@idea-inspired.com>
2022-12-27 02:14:08 +0100

    day6

---
+++ day6/part1.js
@@ -0,0 +1,20 @@
+const { readLinesFromFile } = require("../utils");
+
+const lines = readLinesFromFile("input.txt");
+
+const bufferSize = 4;
+let buffer;
+
+const results = lines.map((line, index) => {
+  const stream = line.split("");
+  console.log("stream:", line);
+  for (let i = 0; i < stream.length; i++) {
+    buffer = stream.slice(i, i + bufferSize);
+    const set = new Set(buffer);
+    if (set.size === bufferSize) {
+      const result = i + bufferSize;
+      console.log("result", result);
+      return result;
+    }
+  }
+});
